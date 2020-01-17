9d38e7b33dd58cca203fbabfbb2bd8787cdb5bd3
karelskiy <matiuk.dm@gmail.com>
2020-01-17 13:50:03 +0200

    create APIContainer over FindUsers

--- src/components/FindUsers/FindUsers.js
+++ src/components/FindUsers/FindUsers.js
@@ -5,63 +5,16 @@ import classes from './FindUsers.module.css';
 
 
 
-class FindUsers extends Component {
-    constructor(props) {
-        super(props);
-        if(this.props.usersData.length === 0) {
-            axios.get(`https://randomuser.me/api/?results=4&inc=name,location,phone,picture,gender,email&nat=us`).then(response => {
-                this.props.loadFriends(response.data.results);
-            });
-        }
-        this.handle = this.handle.bind(this);
-    //     this.clickOnPage = this.clickOnPage.bind(this)
-    }
+let FindUsers = (props) => {
 
-   
+    let users = props.usersData.map(el => (<Users follow={props.follow} id={el.id} key={el.id} status={el.gender} src={el.picture.large} name={el.name.first + ' ' + el.name.last} city={el.location.city} country={el.location.state} email={el.email} phone={el.phone} />))
 
-    handle() {
-        axios.get(`https://randomuser.me/api/?results=4&inc=name,location,phone,picture,gender,email&nat=us`).then(response => {
-            this.props.loadFriends(response.data.results);
-        });
-    }
-
-    // clickOnPage(i) {
-    //     this.props.clickOnPage(i);
-    //     axios.get(`https://randomuser.me/api/?results=${this.props.pageSize}&page=${i}&inc=name,location,phone,picture,gender,email&nat=us`).then(response => {
-    //         this.props.loadFriends(response.data.results);
-    //     });
-    // }
-
-
-    render() {
-        let users = this.props.usersData.map(el => (<Users follow={this.props.follow} id={el.id} key={el.id} status={el.gender} src={el.picture.large} name={el.name.first + ' ' + el.name.last} city={el.location.city} country={el.location.state} email={el.email} phone={el.phone} />))
-        // let countPage = Math.ceil(this.props.totalPageCount / this.props.pageSize);
-
-        // let arr = [];
-        // for (let i = 1; i <= countPage; i++) {
-        //     arr.push(i)
-        // }
-
-        return (
-            <div>
-                {/* <div className={classes.pages}>
-                    {arr.map(i => {
-                        return <span id={i} onClick={() => this.clickOnPage(i)} className={this.props.choosedPage === i && classes.choosedPage}>{i}</span>
-                    })}
-                </div> */}
-
-                {users}
-                <button onClick={this.handle} className={classes.loadButton}>Load</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            {users}
+            <button onClick={props.handle} className={classes.loadButton}>Load</button>
+        </div>
+    )
 }
 
 export default FindUsers
-
-
-    // props.loadFriends([
-    //     { id: 1, src: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg', status: true, name: 'Svetlana D.', desire: 'I`m looking for a job right now...', location: { country: 'Belarus', city: 'Minsk' } },
-    //     { id: 2, src: 'https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg', status: false, name: 'Mihail I.', desire: 'I like football!!!', location: { country: 'Ukraine', city: 'Kiev' } },
-    //     { id: 3, src: 'https://mdbootstrap.com/img/Photos/Avatars/img(30).jpg', status: false, name: 'Katarina St.', desire: 'Why do guys younger than me like me?', location: { country: 'England', city: 'London' } },
-    // ]
--- src/components/FindUsers/FindUsersContainer.js
+++ src/components/FindUsers/FindUsersContainer.js
@@ -1,6 +1,36 @@
 import { connect } from 'react-redux';
 import FindUsers from './FindUsers';
 import { findUsersActionCreator, loadFriendsActionCreator, clickOnPageActionCreator } from '../../redux/findUsers-reducer';
+import { Component } from 'react';
+import * as axios from 'axios';
+import React from 'react';
+
+class findUsersAPIContainer extends Component {
+    constructor(props) {
+        super(props);
+        if (this.props.usersData.length === 0) {
+            axios.get(`https://randomuser.me/api/?results=4&inc=name,location,phone,picture,gender,email&nat=us`).then(response => {
+                this.props.loadFriends(response.data.results);
+            });
+        }
+        this.handle = this.handle.bind(this);
+    }
+
+    handle() {
+        axios.get(`https://randomuser.me/api/?results=4&inc=name,location,phone,picture,gender,email&nat=us`).then(response => {
+            this.props.loadFriends(response.data.results);
+        });
+    }
+
+    render() {  
+        return (
+            <FindUsers usersData={this.props.usersData}
+                        follow={this.props.follow}
+                        handle={this.handle}/>
+
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -13,17 +43,17 @@ let mapStateToProps = (state) => {
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        follow(id){
+        follow(id) {
             dispatch(findUsersActionCreator(id))
         },
-        loadFriends(users){
+        loadFriends(users) {
             dispatch(loadFriendsActionCreator(users))
         },
-        clickOnPage(id){
+        clickOnPage(id) {
             dispatch(clickOnPageActionCreator(id))
         }
     }
 }
 
-let FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(FindUsers) 
+let FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(findUsersAPIContainer)
 export default FindUsersContainer
